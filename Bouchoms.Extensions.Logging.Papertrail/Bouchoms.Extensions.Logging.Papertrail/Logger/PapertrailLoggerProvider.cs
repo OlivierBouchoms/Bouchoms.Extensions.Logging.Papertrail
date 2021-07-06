b45963d501992c2638fcd37f6760f0ee4164e982
Olivier Bouchoms <olivier_2000@live.nl>
2021-07-06 19:45:45 +0200

    Release 1.0.0 (#3)

---
+++ Bouchoms.Extensions.Logging.Papertrail/Bouchoms.Extensions.Logging.Papertrail/Logger/PapertrailLoggerProvider.cs
@@ -0,0 +1,22 @@
+using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Bouchoms.Extensions.Logging.Papertrail
+{
+    public class PapertrailLoggerProvider : ILoggerProvider
+    {
+        private readonly PapertrailOptions _currentConfig;
+        private readonly ConcurrentDictionary<string, PapertrailLogger> _loggers = new();
+
+        public PapertrailLoggerProvider(IOptionsMonitor<PapertrailOptions> config) => _currentConfig = config.CurrentValue;
+
+        public ILogger CreateLogger(string categoryName) =>
+            _loggers.GetOrAdd(categoryName, name => new PapertrailLogger(name, _currentConfig));
+
+        public void Dispose()
+        {
+            _loggers.Clear();
+        }
+    }
+}
---
+++ Bouchoms.Extensions.Logging.Papertrail/Bouchoms.Extensions.Logging.Papertrail/Options/PapertrailOptions.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Logging;
+
+namespace Bouchoms.Extensions.Logging.Papertrail
+{
+    public class PapertrailOptions
+    {
+        [Required]
+        public string AccessToken { get; set; }
+        
+        [Required]
+        public string Url { get; set; }
+        
+        [Required]
+        public LogLevel LogLevel { get; set; }
+    }
+}
