ebbc372412d944ff8461966a0d8ead51ce4a347e
Olivier Bouchoms <bouchoms.olivier@gmail.com>
2023-03-18 11:59:29 +0100

    Add sample project

---
+++ Bouchoms.Extensions.Logging.Papertrail/Bouchoms.Extensions.Logging.Papertrail.Sample.Net6/Program.cs
@@ -0,0 +1,37 @@
+using Bouchoms.Extensions.Logging.Papertrail;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables ();
+
+// Add services to the container.
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddPapertrail();
+
+builder.Services.AddOptions<PapertrailOptions>()
+                .Bind(builder.Configuration.GetSection(PapertrailOptions.ConfigurationSection))
+                .ValidateDataAnnotations();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
